ca51a4883198a73bf899eed1e066944ba71c4cd7
Barbaros Yildirim <barbarosaffan@gmail.com>
2021-06-27 20:41:26 +0300

    First version

---
+++ main.js
@@ -0,0 +1,10 @@
+// Button navigation functionality
+
+         // declaring variables
+
+let resume = document.getElementById("downloadResume");
+let linkedIn = document.getElementById("linkedInButton");
+let gitHub = document.getElementById("gitHubButton");
+let twitter = document.getElementById("twitterButton");
+let telegram = document.getElementById("telegramButton");
+
